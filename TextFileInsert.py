a456177f29bb248055f509038a5bb4fb4f7f0b55
Gürol Güngör <gurol.gungor7@gmail.com>
2022-10-17 00:10:12 +0300

    Güncelleme

---
+++ TextFileInsert.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        TextFileCreate.py
+# Purpose:
+#
+# Author:      Gürol Güngör
+#
+# Created:     16.10.2022
+# Copyright:   (c) Gürol Güngör 2022
+# Licence:     GNU - General Public Licence
+#-------------------------------------------------------------------------------
+
+#Text dosyası yeri ve adi tanımlanır
+TextDosyasi = r"C:\Database\TextFile.txt"
+
+#Text dosya olusturuluyor.
+f = open(TextDosyasi,"w")
+
+#Text dosyaya kayit yaziliyor
+f.write("Merhaba Dünya")
+
+#islem detayi yazilir.
+print(f)
+
+#Text dosyayı kapatıyoruz
+f.close()
+
